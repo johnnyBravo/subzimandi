aaf5049917d6ff3ca24a67f4f00d384c47b8a8bc
aatishm <aatish.mandelecha@gmail.com>
2015-02-04 21:13:49 -0800

    Basic Infrastructural Stuff 1) Add basic ejs/express stuff 2) Have basic home jumbotron page from twitter bootstrap 3) Have FB Login button on the page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express();
 
+app.set('view engine', 'ejs')
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index');
 })
 
 app.listen(app.get('port'), function() {
